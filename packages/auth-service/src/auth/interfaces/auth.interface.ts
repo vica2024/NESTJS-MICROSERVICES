2abef74d189548ebae9a82a83afdc088af7f9454
awinweaka@gmail.com <awinweaka@gmail.com>
2026-01-29 10:41:37 +0800

    v0.1

--- packages/auth-service/src/auth/interfaces/auth.interface.ts
+++
@@ -1,8 +0,0 @@
-import { Observable } from 'rxjs';
-
-export interface AuthServiceClient {
-  ValidateUser(data: { username: string; password: string }): Observable<{
-    success: boolean;
-    message: string;
-  }>;
-}
--- packages/gateway-service/src/user/interfaces/auth.interface.ts
+++
@@ -1,9 +0,0 @@
-import { Observable } from 'rxjs';
-
-export interface AuthServiceClient {
-  ValidateUser(data: { username: string; password: string }): Observable<{
-    success: boolean;
-    message: string;
-    access_token: string;
-  }>;
-}
